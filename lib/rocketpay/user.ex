372c59c901eee170dd4d987680101a61798df00d
Pedrofiigueiredo <mnopedrodias@gmail.com>
2021-02-23 20:03:15 -0300

    02 - db conn, migrations, create user route

---
+++ lib/rocketpay/user.ex
@@ -0,0 +1,43 @@
+defmodule Rocketpay.User do
+  # Traz as funcionalidades do Ecto para esse módulo
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  # @primary_key -> variável do módulo para o id do tipo UUID
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :age, :email, :password, :nickname]
+
+  schema "users" do
+    field :name, :string
+    field :age, :integer
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :nickname, :string
+
+    timestamps()
+  end
+
+  # Validar e mapear (cast para a tabela) dados
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> unique_constraint([:nickname])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    # Func change() -> recebe um changeset e altera
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
